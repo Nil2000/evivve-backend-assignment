2840cacc6acfb3cd970f12ad4da9ffdc28a061cf
NIL2000 <nilabhra09b.net@gmail.com>
2023-12-21 21:01:18 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/room", (req, res) => {
+  res.sendFile(__dirname + "/room.html");
+});
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  //room connection
+  socket.on("join room", (room) => {
+    socket.join(room);
+    console.log(`User joined room ${room}`);
+  });
+
+  //message sending event
+  socket.on("chat message", (msg, room) => {
+    io.to(room).emit("chat message", msg);
+  });
+
+  //disconnect event
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
